789cb749fecd58f53d81a185c547f1fda697f9f6
Andrey Medvedev <andr.medv.spb@gmail.com>
2015-12-29 17:05:49 +0300

    added callback to sound manager

--- sound_test.py
+++ sound_test.py
@@ -1,8 +1,11 @@
+from __future__ import print_function
 from SoundManager import SoundManager
 
 sm = SoundManager()
 sm.daemon = True
 sm.start()
 
-sm.play_sound('coin.wav', lambda: sm.play_sound('coin.wav'))
+sm.play_sound('robot_1.wav')
 
+while True:
+    pass
